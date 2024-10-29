59e35ea949768ae69de590dfb9e74dd9fe9f83ec
David Kwon <david.kwon@clover.com>
2024-10-29 15:43:31 -0600

    Add test cases for Bag class

---
+++ __tests__/Bag.test.js
@@ -0,0 +1,44 @@
+const Bag = require("../classes/Bag");
+const Person = require("../classes/Person");
+const { describe, it, expect } = require("@jest/globals");
+
+describe("Bag tests", () => {
+    let testBagNew;
+    let testBagOwned
+    let testPerson;
+    let testOwner;
+    beforeEach(() => {
+        testBagNew = new Bag(10, 17);
+        testPerson = new Person("Dave", "New York");
+        testOwner = new Person("Brandon", "Los Angeles");
+        testBagOwned.assignOwner(testOwner);
+    })
+
+    it("Should create instance of Bag class", () => {
+        expect(testBag).toBeInstanceOf(Bag);
+    })
+
+    describe("Bag properties", () => {
+        it("Should have weight and id properties", () => {
+            expect(testBagNew).toHaveProperty("weight", 10);
+            expect(testBagNew).toHaveProperty("id", 17);
+        }) 
+        
+        it("Should have private owner property with initial value of null", () => {
+            expect(testBagNew.getOwner()).toBe(null);
+        })
+    })
+
+    describe("Bag methods", () => {
+        it("Should get correctly retrieve owner with getOwner()", () => {
+            expect(testBagOwned.getOwner()).toBeInstanceOf(Person);
+            expect(testBagOwned.getOwner()).toEqual(testOwner);
+        })
+        
+        it("Should correctly set owner with assignOwner()", () => {
+            testBagNew.assignOwner(testPerson);
+            expect(testBag.getOwner()).toBeInstanceOf(Person);
+            expect(testBag.getOwner()).toEqual(testPerson);
+        })
+    })
+})
